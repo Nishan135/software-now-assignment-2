36493dbf4bcf090ff1f49baaa3b11290fec2041b
Bhuwan-giri <s397414@students.cdu.edu.au>
2026-01-14 19:34:31 +0930

    user input

--- assignment2 QN3/mainq3.py
+++ assignment2 QN3/mainq3.py
@@ -3,7 +3,7 @@ from recursive import edge
 
 
     
-def main (): #Defening `main` function
+def main (): #Defening ``
 
   screen = turtle.Screen() 
   screen.title("Recursive Pattern Drawer") 
@@ -15,7 +15,7 @@ def main (): #Defening `main` function
   pen.color("black") 
   pen.pensize(1) 
   
- 
+        #user inputs 
   try:
     sides = int(input("Enter the number of sides:"))
     length = float(input("Enter the side length:"))
