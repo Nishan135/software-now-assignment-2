6eb374fdfc17de3ef810199a6fe76251df9c421f
Bhuwan-giri <s397414@students.cdu.edu.au>
2026-01-14 19:37:05 +0930

    comments of different section are added

--- assignment2 QN3/mainq3.py
+++ assignment2 QN3/mainq3.py
@@ -3,7 +3,7 @@ from recursive import edge
 
 
     
-def main (): #Defening ``
+def main (): #Defening the main function.
 
   screen = turtle.Screen() 
   screen.title("Recursive Pattern Drawer") 
@@ -15,7 +15,7 @@ def main (): #Defening ``
   pen.color("black") 
   pen.pensize(1) 
   
-        #user inputs 
+        #user inputs from the user for sides, length and depth.
   try:
     sides = int(input("Enter the number of sides:"))
     length = float(input("Enter the side length:"))
@@ -30,7 +30,7 @@ def main (): #Defening ``
   pen.pendown() 
   
   
-  angle = 360 / sides 
+  angle = 360 / sides   #Calculating the angle based on the number of sides given by the user.
   for _ in range(sides): 
     edge(pen,length, depth)
     pen.right(angle) 
